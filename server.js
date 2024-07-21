c77a586cfb5ca3dded6c526aa5d985e2fb7344ff
MateusProjetos <157444313+MateusProjetos@users.noreply.github.com>
2024-07-21 14:10:21 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let browser, page;
+
+const initializePuppeteer = async () => {
+    browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
+};
+
+initializePuppeteer().then(() => {
+    console.log('Puppeteer initialized');
+    const videoRoutes = require('./routes/videoRoutes')(page);
+    app.use('/api/videos', videoRoutes);
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}).catch(error => {
+    console.error('Failed to initialize Puppeteer:', error);
+});
+
+process.on('exit', () => {
+    if (browser) {
+        browser.close();
+    }
+});
